a6d4d381dad0c24f981720de705b38caced685d3
SgtMoloch <sgtmoloch@gmail.com>
2024-08-18 08:10:30 -0400

    add harmony patch to address issue

---
+++ DW5CoalMine/DW5CoalMine.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using Game.Events;
+using HarmonyLib;
+using Railloader;
+using Serilog;
+
+
+namespace DW5CoalMine
+{
+    public class DW5CoalMine : PluginBase
+    {
+        ILogger logger = Log.ForContext<DW5CoalMine>();
+        private readonly IModdingContext ctx;
+        private readonly IModDefinition self;
+
+        public DW5CoalMine(IModdingContext ctx, IModDefinition self)
+        {
+            this.ctx = ctx;
+            this.self = self;
+            new Harmony(self.Id).PatchAll(GetType().Assembly);
+        }
+    }
+}
