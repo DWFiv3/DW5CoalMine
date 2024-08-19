c15e8d005ae99d4988aa5287e67526798313fe8d
DWFiv3 <dale@dwfiv3.com>
2024-08-19 18:22:39 +1000

    v2.0.1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-404 README not found
+# **DWFiv3s Coal Mine**
+
+***v2.0.1**
+- Expands out the Incoming Tracks to provide well over 100 incoming cars
+- Expands mining supplies to accomidate more (you'll need it!)
+- 6 Outbound Storage Tracks, the "hump yard" now recieves the cars directly, can store over 100 cars
+- Production has been sped up past that of Nick's Production mod, so you'll be busy
+- Removed the "Cheat Tunnel" 
+- Lengthended the hump yard to accomicdate mote
+
+Special thanks to Sgt Moloch for assisting in the teleportation side, wouldnt have been possible otherwise.
---
+++ SgtMoloch Teleportation /DW5CoalMine/TeleportLoadingIndustryPatch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using Model;
+using Model.OpsNew;
+using Serilog;
+using Track;
+
+namespace DW5CoalMine
+{
+
+    [HarmonyPatch(typeof(TeleportLoadingIndustry))]
+    [HarmonyPatch("CheckRouteClear")]
+    internal class TeleportLoadingIndustryPatch_CheckRouteClear
+    {
+        static Serilog.ILogger logger = Log.ForContext(typeof(TeleportLoadingIndustryPatch_CheckRouteClear));
+
+        private static bool Prefix(ref Location start, Location end)
+        {
+
+            if (end.segment.id.Contains("DW5Seg"))
+            {
+                if (start.segment.id.Contains("Saqq") || start.segment.id.Contains("S66b") || start.segment.id.Contains("DW5Seg"))
+                {
+                    logger.Information("DW5CoalMine is present, setting the start location to the end location due to how this method works.");
+                    start = end;
+                }
+            }
+
+            return true;
+        }
+    }
+}
